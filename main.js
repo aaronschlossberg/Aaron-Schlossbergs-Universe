7ac8b37d895e7b202129be416d332e30583e838c
Aaron Schlossberg <aschloss04@gmail.com>
2025-11-06 00:57:59 -0500

    so much, improved the site

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function copyToClipboard(text, type) {
-  navigator.clipboard.writeText(text).then(() => {
-      alert(type + " has been copied to clipboard! \n\n" + type + ": " + text);
-  }).catch(err => {
-      console.error("Failed to copy: ", err);
-  });
+    navigator.clipboard.writeText(text).then(() => {
+        alert(type + " has been copied to clipboard! \n\n" + type + ": " + text);
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
 }
